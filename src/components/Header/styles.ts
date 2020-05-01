af7822d983157b2d3443d6bdbbdf3056d0a8a0dd
Bruno Lacerda <brunolcpg@gmail.com>
2020-05-01 18:52:05 -0300

    Add functions to list transactions and upload file

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -22,6 +22,7 @@ export const Container = styled.div<ContainerProps>`
         text-decoration: none;
         font-size: 16px;
         transition: opacity 0.2s;
+        position: relative;
 
         & + a {
           margin-left: 32px;
@@ -29,6 +30,16 @@ export const Container = styled.div<ContainerProps>`
 
         &:hover {
           opacity: 0.6;
+        }
+
+        &.active:after {
+          content: '';
+          width: 73px;
+          height: 2px;
+          background-color: #ff872c;
+          position: absolute;
+          top: 27px;
+          left: 0;
         }
       }
     }
